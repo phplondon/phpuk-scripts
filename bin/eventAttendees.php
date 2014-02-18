da70a73a4e4937b16f92f0de9a2bdc76ee80eb6c
arron <arron@copiadigital.co.uk>
2014-02-18 17:16:56 +0000

    Store checked in boolean, and time

--- bin/eventAttendees.php
+++ bin/eventAttendees.php
@@ -14,18 +14,32 @@ $eb = new Php2014\EventBrite($authentication_tokens);
 $result = $eb->event_list_attendees(array(
     'id' => $config['eventbrite']['eventId'],
     'status' => 'attending',
+    'only_display' => 'last_name,address,profile,company,age,gender,barcodes',
+    'show_full_barcodes' => 'true'
         ));
 
 $data = array();
 foreach ($result->attendees as $object) {
     $attendee = $object->attendee;
+
+    $checkInTime = null;
+    foreach ($attendee->barcodes as $barcodeObject) {
+        $barcode = $barcodeObject->barcode;
+        if ((string) $barcode->status == 'used') {
+            $date = strtotime($barcode->date_modified) + 8 * 3600;
+            $floor = floor($date / 3600) * 3600;
+            $checkInTime = date('c', $floor);
+        }
+    }
+
     $data[$attendee->id] = array(
         'lastname' => $attendee->last_name,
         'country' => empty($attendee->work_country_code) ? 'n/a' : $attendee->work_country_code,
         'company' => empty($attendee->company) ? 'n/a' : $attendee->company,
         'age' => empty($attendee->age) ? 'n/a' : $attendee->age,
         'gender' => empty($attendee->gender) ? 'n/a' : $attendee->gender,
-        'checkedInAt' => null,
+        'checkedIn' => ($checkInTime) ? 1 : null,
+        'checkedInAt' => $checkInTime,
         'i' => 1
     );
 }
